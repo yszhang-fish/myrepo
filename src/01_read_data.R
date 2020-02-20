fa508f5ba7807af3b1747f0cf132fddcd6015339
yszhang-fish <zhangyins@gmail.com>
2020-02-20 15:22:02 -0500

    remove test comment create merge conflict

--- src/01_read_data.R
+++ src/01_read_data.R
@@ -7,11 +7,9 @@ kahuna <- read_csv(here::here('data', '2018-11-26_2017-Cape-Hatteras-BRS-kahuna-
 kStart <- kahuna %>% 
   filter(status == 'start')
 
-# Read in Gm182 Data: 100 estimated positions of Gm182, augmented with focal follow data
 gm182UP <- read_csv(here::here('data', '2018-11-27_Gm182-UserPoints-Start-CEE-Locations-Kahuna.csv')) %>% 
   mutate(status = 'userPoints')
 
 # Read in Gm182 Data: 100 estimated positions of Gm182
 gm182 <- read_csv(here::here('data', '2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv')) %>% 
   mutate(status = 'noUserPoints')
-
